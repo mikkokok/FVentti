2b4879b0145e667d092bce93d4d4135bf70dfe3b
Mikko Kokkonen <mikko.kokkonen@olen.to>
2017-05-28 21:03:16 +0300

    Paremmat tulostukset ja testauksia.

--- FVentti/Program.fs
+++ FVentti/Program.fs
@@ -22,11 +22,11 @@ type Numero = // Numero on discriminated union, union Numero + Maa
 
 type Pelaaja = {
                 Nimi : String;
-                Kasi : Numero list
+                Kasi : Numero list;
                 }
 
 // Luo korttipakka
-// Korttipakka on Kortti list
+// Korttipakka on Numero list
 let Korttipakka = [ for maa in [Maa.Hertta; Maa.Pata; Maa.Ruutu; Maa.Risti] do
                     yield Assa(maa)
                     yield Kaksi(maa)
@@ -42,25 +42,27 @@ let Korttipakka = [ for maa in [Maa.Hertta; Maa.Pata; Maa.Ruutu; Maa.Risti] do
                     yield Kuningatar(maa)
                     yield Kuningas(maa)
                    ]
-// Funktiot
 
+
+// Funktiot
+// %A discminited unioneille
 // Kerro pakka
-let kerroPakka =
-    printf "----------Korttipakan kortit---------------------------"
-    for kortti in Korttipakka do 
-    printf "%O \n" kortti
+let kerroPakka pakka =
+    printf "----------Korttipakan kortit--------------------------- \n"
+    for kortti in pakka do 
+    printf "%A \n" kortti
 
 // Kerro pelaajan käsi
 let kerroPelaaja pelaaja = 
-    printf "----------Pelaajan kortit---------------------------"
+    printf "----------Pelaajan kortit--------------------------- \n"
     for kortti in pelaaja do
-    printf "%O" kortti
+    printf "%A \n" kortti
 
 // Kerro jakajan käsi
 let kerroJakaja jakaja =
-    printf "----------Jakajan kortit---------------------------"
+    printf "----------Jakajan kortit--------------------------- \n"
     for kortti in jakaja do
-    printf "%O" kortti
+    printf "%A \n" kortti
 // Sekoita korttipakka
 
 let sekoitaPakka pakka =
@@ -68,7 +70,7 @@ let sekoitaPakka pakka =
     pakka |> List.sortBy (fun kortti -> random.Next())
 
 // Jaa kortti jakajalle ja pelaajalle eli aloita peli
-
+// Palauttaa tuplen (kortti, pakka)
 let jaaKortti pakka = 
     match pakka with
     | kortti::loppuPakka -> (Some kortti, loppuPakka)
@@ -77,13 +79,33 @@ let jaaKortti pakka =
 let annaKorttiPakasta pakka = 
     Korttipakka.Head
 
+
 // Tulosta ja toimita
 let main() = 
     let pakka = sekoitaPakka(Korttipakka)
 
-    let mutable jakaja = {Nimi = "Jakaja";  Kasi = List.empty}
-    let mutable pelaaja = {Nimi = "Pelaaja"; Kasi = List.empty}
-    kerroPakka // Lukee pakan
+    let jakaja = {Nimi = "Jakaja";  Kasi = List.empty}
+    let pelaaja = {Nimi = "Pelaaja"; Kasi = List.empty}
+    kerroPakka pakka // Lukee pakan
+
+    // Testausta varten
+    let (kortti, pakka) = jaaKortti pakka
+    let (korttia, pakka) = jaaKortti pakka
+    let (korttib, pakka) = jaaKortti pakka
+    let (korttic, pakka) = jaaKortti pakka
+    let (korttid, pakka) = jaaKortti pakka  
+    
+    printf "%A \n" kortti
+    printf "%A \n" korttia
+    printf "%A \n" korttib
+    printf "%A \n" korttic
+    printf "%A \n" korttid
+    printf "------------------------------------------------ \n"
+    kerroPakka pakka // Lukee pakan
+    
+    
+
+    
     
     //jakaja.Kasi = List.append(jaaKortti(pakka))
     //jakaja.Kasi = List.append(jaaKortti(pakka))
